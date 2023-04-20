0c5127786ddb561190e960fc963909ae90559cf7
philipheferreira <philipheferreira@gmail.com>
2023-04-20 14:33:28 -0300

    primeiro comentario

---
+++ src/components/contador.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   <h2>Contador: {{ contador }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'contadorPag', 
+    data: () => ({
+      contador: 0
+    })
+}
+</script>
+
+<style>
+    .cont {
+        color: blue;
+    }
+</style>
