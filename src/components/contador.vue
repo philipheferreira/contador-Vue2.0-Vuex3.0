621458726eeee3c97934a1e3e92b321953e2f485
philipheferreira <philipheferreira@gmail.com>
2023-04-20 14:55:35 -0300

    update contadorPag metodos incremento e decremento

--- src/components/contador.vue
+++ src/components/contador.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
    <h2>Contador: {{ contador }}</h2>
+   <button @click="decremento">Decremento</button>
+   <button @click="incremento">Incremento</button>
   </div>
 </template>
 
@@ -10,7 +12,15 @@ export default {
     name: 'contadorPag', 
     data: () => ({
       contador: 0
-    })
+    }), 
+    methods: {
+      incremento: function( ) {
+        this.contador++
+      }, 
+      decremento: function( ) { 
+        this.contador--
+      }
+    }
 }
 </script>
 
